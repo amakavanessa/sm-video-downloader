055ce4b89d3c0cb5d0b3d66af0ceb272bfe994d8
Precious Nnam <nnamvannessa@gmail.com>
2023-03-22 22:08:49 +0100

    worked on the view so that the downloaded video files can be displayed on the browser user interface

--- app.js
+++ app.js
@@ -5,34 +5,39 @@ const axios = require("axios");
 const path = require("path");
 
 const app = express();
-
+const files = fs.readdirSync("static/downloaded files/");
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "static")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  // res.render("index");'
+  res.render("index", { files });
 });
 
 app.post("/search", async (req, res) => {
   const { query } = req.body;
+  const randomNum = Math.floor(Math.random() * (1000 - 0 + 1)) + 0;
+
   const browser = await puppeteer.launch();
+
   const page = await browser.newPage();
 
-  await page.setDefaultNavigationTimeout(0);
+  page.setDefaultNavigationTimeout(0);
 
-  // Navigate to the Instagram video page.
+  // Navigate to the Twitter tweet where the video is located.
   await page.goto(query);
 
   // Wait for the video element to be loaded.
-  await page.waitForSelector("video");
+  await page.waitForSelector("video", {
+    waitUntil: "networkidle2",
+  });
 
   // Get the source URL of the video.
   const videoUrl = await page.$eval("video", (el) => el.src);
-
+  console.log(`${videoUrl} and ${query}`);
   // Use axios to download the video content as a stream.
-
   const videoStream = await axios({
     url: videoUrl,
     method: "GET",
@@ -41,16 +46,16 @@ app.post("/search", async (req, res) => {
 
   // Use Node.js built-in `fs` module to write the video stream to a file.
 
-  const randomNum = Math.floor(Math.random() * (1000 - 0 + 1)) + 0;
   const videoFile = fs.createWriteStream(
-    `downloaded files/video${randomNum}.mp4`
+    `static/downloaded files/video${randomNum}.mp4`
   );
+
   videoStream.data.pipe(videoFile);
   videoFile.on("finish", () => {
     console.log("Video downloaded successfully!");
   });
 
-  await browser.close();
+  browser.close();
 });
 
 const server = app.listen(3000, () => {
